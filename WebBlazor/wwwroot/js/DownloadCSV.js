fb17bf3497f71137acc56173593a4c3a11d01fce
Isra8Rubio <isra-rubio@hotmail.com>
2025-09-15 09:57:46 +0200

    CsvHelperCorrecto

---
+++ WebBlazor/wwwroot/js/DownloadCSV.js
@@ -0,0 +1,11 @@
+﻿export function saveFile(filename, content) {
+    const blob = new Blob([content], { type: 'text/csv;charset=utf-8;' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.setAttribute('download', filename);
+    document.body.appendChild(a);
+    a.click();
+    URL.revokeObjectURL(url);
+    a.remove();
+}
